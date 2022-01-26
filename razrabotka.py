de64c9d521bb928cd7102d8f0e1b208249638e22
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2022-01-26 20:08:32 +0300

    Update razrabotka.py

--- razrabotka.py
+++ razrabotka.py
@@ -1,4 +1,4 @@
-def input_R()
+def input_R():
      while True:
         x = int(input('Введите начальное значение = '))
         if x > 0:
@@ -6,15 +6,24 @@ def input_R()
         print('Недопустимое значение')
     return x
 
-def x2()
+def x2(x):
      return collatz(x // 2)
 
 
-def x3_1()
-#Коля
-    return collatz( n*3+1 )
+def x3_1(x):
+    return collatz( x*3+1 )
 
 
-def collatz()
+def collatz(x):
+      result = [x]
+    if x == 1:
+        pass
+    elif x % 2 == 0:
+        result.extend(x2(x))
+    else:
+        result.extend(x3_1(x))
+    return result
 
+n = input_R()
+    print(collatz(n))
 
